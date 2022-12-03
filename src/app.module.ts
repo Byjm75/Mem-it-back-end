2b13b31f297305e8a60fb61d848414a74ceb8e42
Byjm75 <yoanmark75@gmail.com>
2022-12-03 15:39:54 +0100

    V0.1 allpackage installé et bdd et tables crée

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilisateurModule } from './utilisateur/utilisateur.module';
+import { CategorieModule } from './categorie/categorie.module';
+import { TacheModule } from './tache/tache.module';
+import { TagModule } from './tag/tag.module';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 @Module({
-  imports: [],
+  imports: [UtilisateurModule, CategorieModule, TacheModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
