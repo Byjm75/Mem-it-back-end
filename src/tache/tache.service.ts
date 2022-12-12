4372651f2313bbb7a64de628b93c536b4919c728
MattDao <113097684+MattDao@users.noreply.github.com>
2022-12-12 09:25:57 +0100

    maj 12/12

--- src/tache/tache.service.ts
+++ src/tache/tache.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTacheDto } from './dto/create-tache.dto';
 import { UpdateTacheDto } from './dto/update-tache.dto';
+import { Tache } from './entities/tache.entity';
 
 @Injectable()
 export class TacheService {
-  create(createTacheDto: CreateTacheDto) {
-    return 'This action adds a new tache';
+  constructor(
+    @InjectRepository(Tache)
+    private TacheRepository: Repository<Tache>,
+  ) {}
+
+  async create(createTacheDto: CreateTacheDto): Promise<Tache> {
+    return await this.TacheRepository.save(createTacheDto);
+    // Cette action crée un nouveau mémo;
   }
 
-  findAll() {
-    return `This action returns all tache`;
+  async findAll(): Promise<Tache[]> {
+    return await this.TacheRepository.find();
+    // Cette action presente tout les mémos
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tache`;
+  async findOne(idValue: string): Promise<Tache> {
+    const taskFound = await this.TacheRepository.findOneBy({ id: idValue });
+    if (!taskFound) {
+      throw new NotFoundException(`pas de tâches avec l'id:${idValue}`);
+    }
+    return taskFound;
+    //Cette action presente un mémo;
   }
 
-  update(id: number, updateTacheDto: UpdateTacheDto) {
-    return `This action updates a #${id} tache`;
+  async update(id: string, updateTacheDto: UpdateTacheDto): Promise<Tache> {
+    const tacheToUpdate = await this.findOne(id);
+    tacheToUpdate.body = updateTacheDto.body;
+    tacheToUpdate.date_event = updateTacheDto.date_event;
+    tacheToUpdate.image = updateTacheDto.image;
+    tacheToUpdate.title = updateTacheDto.title;
+    tacheToUpdate.url = updateTacheDto.url;
+    return await this.TacheRepository.save(tacheToUpdate);
+    // Cette action met à jour un mémo;
   }
 
-  remove(id: number) {
+  async remove(id: string): Promise<string> {
+    const result = await this.TacheRepository.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`pas de bouquin avec l'id:${id}`);
+    }
     return `This action removes a #${id} tache`;
+    // Cette action supprime un mémo;
   }
 }
--- src/utilisateur/entities/utilisateur.entity.ts
+++ src/utilisateur/entities/utilisateur.entity.ts
@@ -25,21 +25,22 @@ export class Utilisateur {
   }) //je précise le varchar car pas en 255.
   pseudo: string;
   //TypeOrm est typé par default en varchar 255 si autre le préciser
-  @Column({
-    nullable: true,
-  })
-  picture: string;
 
   @Column({
     nullable: false,
   })
   password: string;
 
+  @Column({
+    nullable: true,
+  })
+  picture?: string;
+
   //Je relis les tables suivant leurs cardinalités et par les clés étrangéres.
   @OneToMany(() => Categorie, (categories) => categories.user_)
   categories: Categorie[];
 
-  @OneToMany(() => Tache, (taches) => taches.user_)
+  @OneToMany(() => Tache, (task) => task.user_)
   user_: Tache;
 
   @OneToMany(() => Tag, (userTag) => userTag.user)
