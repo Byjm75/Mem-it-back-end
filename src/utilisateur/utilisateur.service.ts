0d1d1c8f6151de6f89163908052ae82f09db4b83
Mouss <elmost777@gmail.com>
2022-12-19 17:57:54 +0100

    maj version

--- src/utilisateur/utilisateur.service.ts
+++ src/utilisateur/utilisateur.service.ts
@@ -3,6 +3,7 @@ import { CreateUtilisateurDto } from './dto/create-utilisateur.dto';
 import { UpdateUtilisateurDto } from './dto/update-utilisateur.dto';
 import { Utilisateur } from './entities/utilisateur.entity';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class UtilisateurService {
  constructor(
