15f45fa2aa33db132911e56f6d0c9e4c923c8a0b
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2022-12-19 17:35:21 +0100

    MAJ

--- src/utilisateur/utilisateur.service.ts
+++ src/utilisateur/utilisateur.service.ts
@@ -5,23 +5,19 @@ import { UpdateUtilisateurDto } from './dto/update-utilisateur.dto';
 import { Utilisateur } from './entities/utilisateur.entity';
 import { Repository } from 'typeorm';
 @Injectable()
-@Injectable()
 export class UtilisateurService {
-  utilisateurRepository: any;
   //Construction et rappel de la table (utilisateur)
+  constructor(
+    @InjectRepository(Utilisateur)
+    private utilisateurRepository: Repository<Utilisateur>,
+  ) {}
 
   // .Post pour la Création d'un utilisateur avec le rappel des éléments du DTO (interface)
   async create(
     createUtilisateurDto: CreateUtilisateurDto,
   ): Promise<Utilisateur> {
     return await this.utilisateurRepository.save(createUtilisateurDto);
-    //Construction et rappel de la table (utilisateur)
-    UtilisateurService.constructor(
-    @InjectRepository(Utilisateur)
-    private utilisateurRepository: Repository<Utilisateur>,
-  ) { }
   }
-
 
   // .get pour trouver l'ensemble des utilisateurs contenus ds la table utilisateur
   async findAll(): Promise<Utilisateur[]> {
